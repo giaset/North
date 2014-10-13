0827c71ab8d550da0a98aaa61b2c6de2cf796783
Gianni Settino <g@miltonandparc.com>
2014-10-13 17:40:52 -0400

    Holy shit. Fuck storyboards

---
+++ North/MainTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.swift
+//  North
+//
+//  Created by Gianni Settino on 2014-10-13.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+    
+}
